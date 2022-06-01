ecd5a4c24c67a9441ecf309b5da750405a20cd6b
serj <sadas@dsas.ds>
2022-06-01 16:19:59 +0300

    Initial Commit

---
+++ Svetafor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Svetafor
+//
+//  Created by Сергей Коляда on 01/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
